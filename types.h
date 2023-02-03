9558be2789caf2e4d5710305b37dcc7031f6848b
Anna <areyes@milaifontanals.org>
2023-02-03 18:35:37 +0100

    03/02/2023 - FI PRACTCA

--- types.h
+++ types.h
@@ -1,12 +1,12 @@
 #define MAX_CLIENTS 5
-#define PORT 1132
+#define PORT 1133
 #define BACK_LOG 10
 #define PATH "/mnt/hgfs/UF3/1_Sockets/Practica"
 #define PATH_FITXER_IPS "/mnt/hgfs/UF3/1_Sockets/Practica/ips_valides.txt"
 #define PATH_SERVER "/mnt/hgfs/UF3/1_Sockets/Practica/directori_server"
 #define PATH_CLIENT "/mnt/hgfs/UF3/1_Sockets/Practica/directori_client"
 #define PATH_LOG "/mnt/hgfs/UF3/1_Sockets/Practica/log.txt"
-#define IP_SERVER "10.200.2.4" //"192.168.1.153"
+#define IP_SERVER "10.200.2.4"
 
 #define PATH_WINDOWS "C:\\Users\\anna9\\Documents\\ESTUDIOS\\DAM\\2022-2023\\M09-PSP\\UF3\\1_Sockets\\Practica"
 #define PATH_SERVER_WINDOWS "C:\\Users\\anna9\\Documents\\ESTUDIOS\\DAM\\2022-2023\\M09-PSP\\UF3\\1_Sockets\\Practica\\directori_server"
